7e46485117e059b3ff2304e724b53e549eab8262
AnasDalalaah <anasfuad232@gmail.com>
2021-06-01 08:47:56 +0300

    editing JS code to appear Table

--- app.js
+++ app.js
@@ -1,8 +1,165 @@
-'use strict';
+"use strict";
 
-let Hours= ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+const Hours= ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
 console.log(Hours);
-let sum;
+
+let StoreTable = document.getElementById('Cookies-Table');
+
+
+
+
+
+
+
+const Markets = [];
+
+
+
+function LOCATION(City, Min, Max, avg){
+
+    this.City = City;
+    this.Min = Min;
+    this.Max = Max;
+    this.avg = avg;
+    this.Total = 0;
+    this.CustomerPerHour = [];
+    this.CookiesPerHour = [];
+
+    Markets.push(this);
+    console.log(this);
+
+
+    LOCATION.prototype.CustomersPerHour = function () {
+  for (let i = 0; i < Hours.length; i++) {
+    let random = Math.floor(Math.random() * (this.Max - this.Min) + this.Min);
+    this.CustomerPerHour.push(random);
+  }
+
+};
+
+LOCATION.prototype.CookiesCount = function () { 
+  for (let i = 0; i < Hours.length; i++) {
+    let Cookies = this.avg * this.CustomerPerHour[i];
+    this.CookiesPerHour.push(Math.ceil(Cookies));
+  }
+};
+
+LOCATION.prototype.setTotalCookies = function () {
+  for (let i = 0; i < this.CookiesPerHour.length; i++) {
+    this.Total = this.Total + this.CookiesPerHour[i];
+   
+  }
+  return this.Total;
+};
+
+LOCATION.prototype.HTMLDOC = function(){
+  let trelment = document.createElement('tr');
+  let tdelment = document.createElement('td');
+  tdelment.textContent = this.City;
+  trelment.appendChild(tdelment);
+  //trelment.textContent = Hours;
+  //tdelment.appendChild(trelment);
+  let headingRow = document.createElement('tr');
+  StoreTable.appendChild(headingRow);
+  let th1 = ' ';
+  th1 = document.createElement('th');
+  headingRow.appendChild(th1);
+  for (let i = 0; i < Hours.length; i++) {
+     th1 = document.createElement('th');
+     headingRow.appendChild(th1);
+     th1.textContent = `${Hours[i]}`;
+     //   console.log(thElement);
+  //th1.textContent = 'Daily location total';
+}
+
+  for (let i = 0 ; i < this.CookiesPerHour.length ; i++){
+  let tdelment = document.createElement('td');
+  tdelment.textContent = this.CookiesPerHour[i];
+  trelment.appendChild(tdelment);
+ }
+ tdelment = document.createElement('td');
+ tdelment.textContent = this.Total;
+ trelment.appendChild(tdelment);
+
+ StoreTable.appendChild(trelment);
+ 
+}
+
+
+}
+
+
+const  Seattle = new LOCATION("Seattle",23,65, 6.3);
+Seattle.CustomersPerHour();
+Seattle.CookiesCount();
+Seattle.setTotalCookies();
+Seattle.HTMLDOC();
+
+const  Tokyo = new LOCATION("Tokyo", 3 , 24 , 1.2);
+Tokyo.CustomersPerHour();
+Tokyo.CookiesCount();
+Tokyo.setTotalCookies();
+Tokyo.HTMLDOC();
+
+const  Dubai = new LOCATION("Dubai", 11, 38,3.7);
+Dubai.CustomersPerHour();
+Dubai.CookiesCount();
+Dubai.setTotalCookies();
+Dubai.HTMLDOC();
+
+
+
+const  Paris = new LOCATION("Paris", 20,38,2.3 );
+Paris.CustomersPerHour();
+Paris.CookiesCount();
+Paris.setTotalCookies();
+Paris.HTMLDOC();
+
+const  Lima = new LOCATION("Lima",2,16,4.6 );
+Lima.CustomersPerHour();
+Lima.CookiesCount();
+Lima.setTotalCookies();
+Lima.HTMLDOC();
+
+let CITIES=[Seattle,Tokyo,Dubai,Paris,Lima];
+
+total();
+
+function total () {
+let trTotal = document.createElement("tr");
+StoreTable.appendChild(trTotal);
+trTotal.textContent = "Total";
+
+
+let grandSum = 0 ;
+let summationPerour ;
+let td ;
+for (let i=0 ; i< Hours.length ; i++){
+  summationPerour = 0
+  
+  for (let j =0 ; j< CITIES.length ; j++){
+    summationPerour = summationPerour + CITIES[j].CookiesPerHour[i]
+
+  }
+
+  td = document.createElement('td');
+  StoreTable.appendChild(td);
+  td.textContent = summationPerour
+
+   grandSum += summationPerour
+}
+
+td = document.createElement('td') ;
+StoreTable.appendChild(td);
+td.textContent=grandSum;
+
+}
+
+/*
+
+//arrofobjects.totalito();
 let Seattle={
 
 City:'Seattle',
@@ -67,6 +224,7 @@ ul.appendChild(li);
 }
 
 }
+
 
 
 
@@ -354,8 +512,4 @@ let Tokyo={
         Lima.CustomersPerHour();
         Lima.setTotalCookies();
         Lima.HTMLDOC();
-  
-
-
-
-
+        */
